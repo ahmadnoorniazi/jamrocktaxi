22c682dbca6c6fc45d55f6317f99974b6025034a
Ahmad Noor <ahmadnoor@folio3.com>
2021-02-17 20:10:53 +0500

    state changes

--- common/src/other/FareCalculator.js
+++ common/src/other/FareCalculator.js
@@ -1,22 +1,23 @@
 export function FareCalculator(distance, time, rateDetails, fleetsPrices) {
 	let price = null;
-
+console.log('flettttttttttttttttttttts pricesssssssssssssssss',fleetsPrices)
 	if (fleetsPrices && fleetsPrices.items.length && fleetsPrices.items[0].fields) {
 		const dis = parseFloat(distance) / 1000;
+		console.log('dissssssssssssssssssssssssss', dis)
 		const data = fleetsPrices.items.find((item) => item.fields.fleetName === rateDetails.name);
 		const filterPrice =
 			data &&
 			data.fields &&
 			data.fields.fleetPrices.find((fleet) => dis >= fleet.fields.start && dis <= fleet.fields.end);
 
-		if (!filterPrice) {
+		if (!filterPrice) {fareCalculation
 			price = { fields: { price: 0, returnPrice: 0 } };
 			console.log('priceeeeeeeeeee innnnnnnnnnnnnnn', price);
 		} else {
 			price = filterPrice;
 		}
 
-		console.log('priceeeeeeeeeee', price);
+		console.log('priceeeeeeeeeeeeeeeeeeeeeeeeeeeee', price);
 	}
 
 	// let baseCalculated = ((parseFloat(rateDetails.rate_per_kilometer) * (parseFloat(distance) / 1000))) + ((parseFloat(rateDetails.rate_per_hour) * (parseFloat(time) / 3600)));
